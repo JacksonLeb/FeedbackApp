8c5c6b788865d72c8e3307cef033b82addec3045
Jackson Leb <jacksonleb@Jacksons-MacBook-Pro.local>
2022-02-18 18:47:58 -0500

    updated first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import Header from './components/Header'
+import FeedbackItem from './components/FeedbackItem'
+
+
+function App() {
+
+  return (
+    <div>
+      <Header />
+      <div className='container'>
+        <FeedbackItem />
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/data/FeedbackData.js
@@ -0,0 +1,22 @@
+const FeedbackData = [
+    {
+        id: 1,
+        rating: 1,
+        text: 'My name is Jackson Leb and I am coding a React App'
+    },
+    {
+        id: 2,
+        rating: 10,
+        text: 'My name is Jackson Leb and I am coding a React App'
+    },
+    {
+        id: 3,
+        rating: 5,
+        text: 'My name is Jackson Leb and I am coding a React App'
+    },
+    {
+        id: 4,
+        rating: 6,
+        text: 'My name is Jackson Leb and I am coding a React App'
+    }
+]
