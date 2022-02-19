aee8d723e7ab0cff74b5da05b1ec3a170e341d55
Jackson Leb <jacksonleb@Jacksons-MacBook-Pro.local>
2022-02-18 19:27:50 -0500

    Added FeedbackList component

--- src/data/FeedbackData.js
+++ src/data/FeedbackData.js
@@ -1,22 +1,19 @@
 const FeedbackData = [
-    {
-        id: 1,
-        rating: 1,
-        text: 'My name is Jackson Leb and I am coding a React App'
-    },
-    {
-        id: 2,
-        rating: 10,
-        text: 'My name is Jackson Leb and I am coding a React App'
-    },
-    {
-        id: 3,
-        rating: 5,
-        text: 'My name is Jackson Leb and I am coding a React App'
-    },
-    {
-        id: 4,
-        rating: 6,
-        text: 'My name is Jackson Leb and I am coding a React App'
-    }
+  {
+    id: 1,
+    rating: 10,
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+  },
+  {
+    id: 2,
+    rating: 9,
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+  },
+  {
+    id: 3,
+    rating: 8,
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+  },
 ]
+
+export default FeedbackData
