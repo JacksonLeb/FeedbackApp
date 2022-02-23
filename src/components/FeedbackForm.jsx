9f932cbe27671509212e0fd75034b238a7db918f
Jackson Leb <jacksonleb@Jacksons-MacBook-Pro.local>
2022-02-22 21:52:22 -0500

    ability to add feedback

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -4,7 +4,7 @@ import Button from './shared/Button'
 import RatingSelect from './RatingSelect'
 import {useState} from 'react'
 
-function FeedbackForm() {
+function FeedbackForm({handleAdd}) {
     const [text, setText] = useState('')
     const [rating, setRating] = useState('')
     const [btnDisabled, setBtnDisabled] = useState(true)
@@ -27,9 +27,23 @@ function FeedbackForm() {
         setText(e.target.value)
     }
 
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        if (text.trim().length > 10) {
+            const newFeedback = {
+                text,
+                rating,
+            }
+
+            handleAdd(newFeedback)
+
+            setText('')
+        }
+    }
+
   return (
     <Card>
-        <form>
+        <form onSubmit={handleSubmit}>
             <h2>How would you rate your service with us?</h2>
             <RatingSelect select={(rating) => console.log(rating)}/>
             <div className='input-group'>
